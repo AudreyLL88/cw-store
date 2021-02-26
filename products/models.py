d62d8da71565fa60c1294e2693e749690876daf4
AudreyLL88 <a.lloancy@gmail.com>
2021-02-26 13:03:09 +0000

    Add size to product model and size selector to product details

--- products/models.py
+++ products/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
         max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     size = MultiSelectField(choices=Sizes, null=True, blank=True)
     qty = models.IntegerField( default='5')
     price = models.DecimalField(max_digits=6, decimal_places=2)

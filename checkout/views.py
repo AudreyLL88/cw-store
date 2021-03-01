3ce74a8dd853faf05f49d1add89b82de1be161fe
AudreyLL88 <a.lloancy@gmail.com>
2021-03-01 15:24:01 +0000

    Add Stripe elements in static files

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IQ9sYFkqoxPh7nGgkqLNSAzp7MEWh9w8IRZeVkg3MBQhtNFBv1YpP2Q0GutPdRz8wZW4wzmcICSFfq9XAudDOZ700lUlXmBSO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

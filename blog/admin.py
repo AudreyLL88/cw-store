5b58091cf3ec13b33ce356579f6d5f7810d789e7
AudreyLL88 <a.lloancy@gmail.com>
2021-03-08 14:49:52 +0000

    Add Admin blog interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    """ Creates the admin interface for Blog posts """
+    list_display = (
+        'blog_title',
+        'author',
+        'blog_content',
+        'date'
+    )
+
+
+admin.site.register(BlogPost, BlogAdmin)

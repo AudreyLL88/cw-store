8e2966931a0706eb774e48dcd11f45889466eabb
AudreyLL88 <a.lloancy@gmail.com>
2021-03-09 12:29:51 +0000

    Add Contact functionnality for admin in contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Create the admin interface for Contact Forms
+    """
+    readonly_fields = (
+        'subject',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'your_message',
+        'date_sent',
+    )
+
+    list_display = (
+        'subject',
+        'first_name',
+        'email',
+        'date_sent',
+    )
+
+    ordering = ('-date_sent',)
+
+
+admin.site.register(Contact, ContactAdmin)

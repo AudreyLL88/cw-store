57c3d83a2b521e9eb1070ce1692741f930a1c192
AudreyLL88 <a.lloancy@gmail.com>
2021-08-17 20:46:52 +0000

    Change email account for email testing on deployed version.

--- cw_store/settings.py
+++ cw_store/settings.py
@@ -214,7 +214,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'a.lloancy@gmail.com'
+    DEFAULT_FROM_EMAIL = 'chelseawolfestore@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
